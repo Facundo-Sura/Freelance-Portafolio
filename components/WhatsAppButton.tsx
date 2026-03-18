c4bf4c67342092a29efa71feec350b9459d8ca62
facundo sura <facundomesura@gmail.com>
2026-03-18 16:05:50 -0300

    estilo arreglado en el boton de whatsapp

--- components/WhatsAppButton.tsx
+++ components/WhatsAppButton.tsx
@@ -97,7 +97,7 @@ export function WhatsAppButton() {
   };
 
   return (
-    <div className="fixed bottom-6 right-6 z-[100] flex flex-col items-end">
+    <div className="fixed bottom-6 right-6 z-100 flex flex-col items-end">
       <AnimatePresence>
         {isOpen && (
           <motion.div
